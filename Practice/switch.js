b89633a87d1f473a2e7f74a11ee810a0b5c79e04
RishavLC <shrestharishav444@gmail.com>
2026-05-27 22:45:05 +0545

    switchcase

---
+++ Practice/switch.js
@@ -0,0 +1,15 @@
+let day = "Monday";
+switch(day) {
+  case "Monday":
+    console.log("Start of the week!");
+    break;
+  case "Friday":
+    console.log("Weekend is near!");
+    break;
+  default:
+    console.log("Just another day.");
+}
+
+
+// let name = prompt("Enter your name:");
+// console.log("Hello, " + name + "!"); prompt is not running in terminal
