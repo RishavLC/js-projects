b0e057b756b01a15bbde7279a417c0e2daf01db6
RishavLC <shrestharishav444@gmail.com>
2026-06-03 22:58:44 +0545

    smallest.js number comparison

---
+++ Practice/smallest.js
@@ -0,0 +1,9 @@
+let a=3
+let b=-8
+if (a < b) {
+    console.log("a is the smallest")
+} else if (b < a) {
+    console.log("b is the smallest")
+} else {
+    console.log("Both numbers are equal.")
+}
