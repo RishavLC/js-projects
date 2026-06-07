b54ee93a1ad3b283fecbaf923afd0f485822948a
RishavLC <shrestharishav444@gmail.com>
2026-06-07 09:23:13 +0545

    reverse a string

---
+++ reverse.js
@@ -0,0 +1,9 @@
+// Reverse a string (supports spaces/punctuation as-is)
+// Usage: node reverse.js "Rishav"
+// If no input is provided, defaults to "Rishav".
+
+const input = process.argv.slice(2).join(" ") || "jhjhjhjhjhjhjhjhjh";
+
+let reversed = input.split("").reverse().join("");
+console.log(reversed);
+
