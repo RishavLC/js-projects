f36be056fa579632fa6bf64012681043c8c1b314
RishavLC <shrestharishav444@gmail.com>
2026-06-07 09:24:45 +0545

    defaulting to "Rishav" if no input is provided

--- reverse.js
+++ reverse.js
@@ -2,7 +2,7 @@
 // Usage: node reverse.js "Rishav"
 // If no input is provided, defaults to "Rishav".
 
-const input = process.argv.slice(2).join(" ") || "jhjhjhjhjhjhjhjhjh";
+const input = process.argv.slice(2).join(" ") || "Rishav";
 
 let reversed = input.split("").reverse().join("");
 console.log(reversed);
