6a86d49ca4cbb4d92f77b1526b20777482af7887
RishavLC <shrestharishav444@gmail.com>
2026-06-13 08:01:43 +0545

    method calculator

--- Practice/methods.js
+++ Practice/methods.js
@@ -7,7 +7,31 @@
 
 // console.log(user.greet());
 
-let arr = [1, 2, 3, 4, 5];
-arr.forEach(function(num) {
-  console.log(num * 2);
-});
+// let arr = [1, 2, 3, 4, 5];
+// arr.forEach(function(num) {
+//   console.log(num * 2);
+// });
+
+//methods
+const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+  divide(a, b) {
+    if (b === 0) {
+      return "Cannot divide by zero";
+    }
+    return a / b;
+  }
+};
+
+console.log(calculator.add(5, 3));
+console.log(calculator.subtract(5, 3));
+console.log(calculator.multiply(5, 3));
+console.log(calculator.divide(5, 0));
