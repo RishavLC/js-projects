f7a84453baa4d7897ecb6648460de78c80727d72
RishavLC <shrestharishav444@gmail.com>
2026-06-14 07:00:28 +0545

    this keyword used for accessing properties

--- Practice/methods.js
+++ Practice/methods.js
@@ -12,26 +12,36 @@
 //   console.log(num * 2);
 // });
 
-//methods
-const calculator = {
-  add(a, b) {
-    return a + b;
-  },
-  subtract(a, b) {
-    return a - b;
-  },
-  multiply(a, b) {
-    return a * b;
-  },
-  divide(a, b) {
-    if (b === 0) {
-      return "Cannot divide by zero";
-    }
-    return a / b;
-  }
+// //methods
+// const calculator = {
+//   add(a, b) {
+//     return a + b;
+//   },
+//   subtract(a, b) {
+//     return a - b;
+//   },
+//   multiply(a, b) {
+//     return a * b;
+//   },
+//   divide(a, b) {
+//     if (b === 0) {
+//       return "Cannot divide by zero";
+//     }
+//     return a / b;
+//   }
+// };
+
+// console.log(calculator.add(5, 3));
+// console.log(calculator.subtract(5, 3));
+// console.log(calculator.multiply(5, 3));
+// console.log(calculator.divide(5, 0));
+
+const person = {
+  name: "Rishav",
+  age: 23,
+  introduce() {
+    return `Hi, I am ${this.name} and I am ${this.age} years old.`;
+  } 
 };
 
-console.log(calculator.add(5, 3));
-console.log(calculator.subtract(5, 3));
-console.log(calculator.multiply(5, 3));
-console.log(calculator.divide(5, 0));
+console.log(person.introduce());
