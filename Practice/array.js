2c3316c7b6fe0c56a592143c4ea1357b763f89a2
RishavLC <shrestharishav444@gmail.com>
2026-06-16 15:06:19 +0545

    accessing elements in an array.

---
+++ Practice/array.js
@@ -0,0 +1,6 @@
+const arra = ["apple", "banana", "cherry"];
+
+// Accessing elements
+console.log(arra[0]); // Output: apple
+console.log(arra[1]); // Output: banana
+console.log(arra[2]); // Output: cherry
