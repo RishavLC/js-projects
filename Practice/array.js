70947b536623064c11a6c33bcb2553486a49b341
RishavLC <shrestharishav444@gmail.com>
2026-06-17 21:38:59 +0545

    push function to add element

--- Practice/array.js
+++ Practice/array.js
@@ -1,6 +1,13 @@
-const arra = ["apple", "banana", "cherry"];
+// const arra = ["apple", "banana", "cherry"];
 
-// Accessing elements
-console.log(arra[0]); // Output: apple
-console.log(arra[1]); // Output: banana
-console.log(arra[2]); // Output: cherry
+// // Accessing elements
+// console.log(arra[0]); // Output: apple
+// console.log(arra[1]); // Output: banana
+// console.log(arra[2]); // Output: cherry
+
+//array method
+const arr = [1, 2, 3, 4, 5];
+
+// push() - adds an element to the end of the array
+arr.push(6);
+console.log(arr); 
