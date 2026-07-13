81bcc0fe238ecfbb5f8d3817bc9806a8ff2fbe24
RishavLC <shrestharishav444@gmail.com>
2026-07-13 21:07:59 +0545

    passing parameters to a function

--- Practice/simplefunction.js
+++ Practice/simplefunction.js
@@ -1,7 +1,12 @@
-function greet(){
+// function greet(){
 
-console.log("Hello");
+// console.log("Hello");
 
-}
+// }
 
-greet();
+// greet();
+
+function greet(name){
+    console.log("Hello " + name);
+}       
+greet("Rishav");
